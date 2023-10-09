6d623664ecbf1bd389c959eb22298f9aade5111c
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-10-09 13:56:33 +0200

    update

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -2,16 +2,17 @@ using Leonardo;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+    
 app.MapGet("/", () => "Hello World! 3");
 
 app.MapGet("/Fibonacci", 
     async () => await Fibonacci.RunAsync(new []{"44", "43"}));
 
-app.Run(context =>
+/*app.Run(context =>
 {
     context.Response.StatusCode = 404;
+    context.Response.BodyWriter.WriteAsync(new byte[] { 0x48, 0x65, 0x6c, 0x6c, 0x6f });
     return Task.CompletedTask;
-});
+});*/
 
 app.Run();
