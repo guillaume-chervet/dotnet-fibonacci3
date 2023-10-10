a3ed21e3972761e49abbfa8ad96049cdf6d4fb33
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-10-10 14:45:25 +0200

    feat(web): hello

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -8,5 +8,6 @@ app.MapGet("/", () => "Hello World! 3");
 app.MapGet("/Fibonacci", 
     async () => await Fibonacci.RunAsync(new []{"44", "43"}));
 
+Console.WriteLine("Hello World!");
 
 app.Run();
