939897f376b1317d5a133bbec94f0ee34381e07e
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-10-10 14:44:04 +0200

    update

--- src/Leonardo.Web/Program.cs
+++ src/Leonardo.Web/Program.cs
@@ -8,11 +8,5 @@ app.MapGet("/", () => "Hello World! 3");
 app.MapGet("/Fibonacci", 
     async () => await Fibonacci.RunAsync(new []{"44", "43"}));
 
-/*app.Run(context =>
-{
-    context.Response.StatusCode = 404;
-    context.Response.BodyWriter.WriteAsync(new byte[] { 0x48, 0x65, 0x6c, 0x6c, 0x6f });
-    return Task.CompletedTask;
-});*/
 
 app.Run();
